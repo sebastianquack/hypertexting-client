5f541ae76b5bc1debb216cbf2653a8d98c5d2212
Sebastian Quack <sebastianquack@gmail.com>
2018-07-19 18:48:00 +0200

    add roomId in socket query

--- src/providers/server/server.ts
+++ src/providers/server/server.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
 
 //var baseUrl = 'http://192.168.1.2:3000';
+//var baseUrl = 'http://localhost:3000';
 var baseUrl = 'https://htserver.herokuapp.com';
 
 /*
@@ -16,7 +17,11 @@ var baseUrl = 'https://htserver.herokuapp.com';
 export class ServerProvider {
 
   socket:any;
+  currentRoomId:string;
+
   constructor(public http: HttpClient) {
+    this.currentRoomId = null;
+
     this.socket = io(baseUrl);
     this.socket.on('disconnect', function(){
       console.log("socket disconnect");
@@ -24,15 +29,26 @@ export class ServerProvider {
     this.socket.on('connect', function(){
       console.log("socket connect");
     });
+    this.socket.on('reconnect_attempt', () => {
+      console.log("reconnect_attempt");
+      this.socket.io.opts.query = {
+        currentRoomId: this.currentRoomId
+      };
+    });
   }
 
   // socket.io api
+
+  setCurrentRoomId(id) {
+    this.currentRoomId = id;
+  }
 
   socketConnected() {
     return this.socket.connected;
   }
 
   joinRoom(room:string) {
+    this.setCurrentRoomId(room);
     this.socket.emit('joinRoom', room); // ask server to put us in a room
   }
 
@@ -45,6 +61,7 @@ export class ServerProvider {
   }
 
   leaveRoom(room:string) {
+    this.setCurrentRoomId(null);
     this.socket.off('message');
     this.socket.emit('leaveRoom', room); // ask server to remove us from a room
   }
